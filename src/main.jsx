6d2adc8fb81d4d263f1ba797f687319ac162171e
Dipanjan Sadhukhan <dipanjansadhukhan123@gmail.com>
2026-05-15 23:32:29 +0530

    Fixed settings for Vercel Deployement

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/my-portfolio/">
+    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
       <App />
     </BrowserRouter>
   </StrictMode>,
